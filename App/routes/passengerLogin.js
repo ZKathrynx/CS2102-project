ee8a518f182b91f474b4fd8d2c60902a2f2890c2
Sha Long <shalong5794@gmail.com>
2019-11-08 22:17:08 +0800

    create index, driver login, passenger login, passenger functions, driver functions

---
+++ App/routes/passengerLogin.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var sql_query = "SELECT * FROM Users WHERE";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('passengerLogin', { title: 'Express' });
+});
+
+// router.post('/', function(req, res, next) {
+// 	// Retrieve Information
+// 	var input_username = req.body.username;
+// 	var input_password = req.body.password;
+	
+// 	// Construct Specific SQL Query
+// 	var select_query = sql_query + "uid = " + input_password;
+	
+// 	pool.query(insert_query, (err, data) => {
+// 		res.redirect('/select')
+// 	});
+// });
+
+module.exports = router;
