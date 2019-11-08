b3697e42d7094604e59fc52d89b7aa345b2d4834
yitong <e0261904@u.nus.edu>
2019-11-08 23:17:19 +0800

    2317

--- App/routes/driverLogin.js
+++ App/routes/driverLogin.js
@@ -1,24 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-var sql_query = "SELECT * FROM Users WHERE";
+const { Pool } = require('pg')
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: '********',
+//   port: 5432,
+// })
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+})
+
+/* SQL Query */
+var sql_query = "SELECT password FROM Driver WHERE";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('driverLogin', { title: 'Express' });
+  res.render('driverLogin', { title: 'Driver Login' });
 });
 
-// router.post('/', function(req, res, next) {
-// 	// Retrieve Information
-// 	var input_username = req.body.username;
-// 	var input_password = req.body.password;
+router.post('/', function(req, res, next) {
+	// Retrieve Information
+	var input_userId = req.body.userId;
+	var input_password = req.body.password;
 	
-// 	// Construct Specific SQL Query
-// 	var select_query = sql_query + "uid = " + input_password;
+	// Construct Specific SQL Query
+	var sql_query = sql_query + "userId = " + input_userId;
 	
-// 	pool.query(insert_query, (err, data) => {
-// 		res.redirect('/select')
-// 	});
-// });
+	pool.query(insert_query, (err, data) => {
+    if (data == input_password) {
+      res.redirect('/driverFunctions')
+    } else {
+      alert("Login failed! Invalid user ID or password")
+    };
+
+	});
+});
 
 module.exports = router;
